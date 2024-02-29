4bc028695340df84480fbad70294fc2d6eba5c73
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-02-29 16:59:41 +0900

    Update .lua

--- .lua
+++ .lua
@@ -3,12 +3,20 @@ local wndw = lib:Window("VIP Turtle Hub V4 - 🤔🤯🤦")
 local T1 = wndw:Tab("Main")
 local T2 = wndw:Tab("Hatch")
 local peler = {}
+local user = game:GetService("Players").LocalPlayer
 local self = {
     remote = {
         click = "",
         hourly = "",
         chest = "",
         egg = ""
+    }
+}
+
+local trigger = {
+    hook = {
+        event = false,
+        functions = false
     }
 }
 
@@ -55,5 +63,31 @@ T2:Toggle("Auto Hatch",false,function(value)
     while wait() do
       if _G.pecah == false then break end
       game:GetService("ReplicatedStorage")["Communication"]["Functions"][self.remote.egg]:InvokeServer("Single",_G.egg)
+    end
+end)
+
+if user.Name == "Rivanda_Cheater" then
+local T3 = wndw:Tab("Developer Access")
+
+T3:Toggle("Trigger any RemoteEvent",false,function(value)
+        trigger.hook.event = value
+end)
+
+T3:Toggle("Trigger any RemoteFunction",false,function(value)
+        trigger.hook.functions = value
+end)
+end
+
+lib:HookCalled(function(self,args)
+     if self.Name == "" and args[1] ~= true and trigger.hook.event == true then
+            while wait() do
+                if trigger.hook.event == false then break end
+                    return self.FireServer(self,unpack(args))
+            end
+    elseif self.Name == "" and args[1] == "Single" and trigger.hook.functions == true then
+            while wait() do
+                if trigger.hook.functions == false then break end
+                    return self.InvokeServer(self,unpack(args))
+            end
     end
 end)
